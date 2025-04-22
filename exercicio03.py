483acbf476bd94d4069c0a27c7adbf01caac87bb
Laís Serafim <laisserafim8@gmail.com>
2025-04-22 09:07:35 -0300

    exercicio03

---
+++ exercicio03.py
@@ -0,0 +1,12 @@
+resposta = "s"
+while resposta == "s":
+    num = float(input("Digite um número: "))
+    if num < 0 and num % 2 ==0:
+        print(f"{num} é Par Negativo")
+    elif num > 0 and num % 2 ==0:
+        print(f"{num} é Par e Positivo ")
+    elif num > 0 and num % 2 == 1:
+        print(f"{num} é impar e Positivo ")
+    else:
+        print(f"{num} é impar e negativo ")
+    resposta = input("Deseja consultar um novo número? Digite S para 'SIM' ")
